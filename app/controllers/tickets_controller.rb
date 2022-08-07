393b3e035c75657b0e874225132901b782edbf4a
Chris C <chris@makecoolstuff.net>
2022-08-07 12:23:50 +1000

    remove reduntant function

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,8 +3,4 @@ class TicketsController < ApplicationController
   def index
     @tickets = Ticket.all
   end
-
-  def react_state
-    { tickets: @tickets.as_json(include: [:creator, :owner]) }
-  end
 end
