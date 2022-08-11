c44e0ff304b7b13083f48ff40649d2eaa451cdcb
Chris C <chris@makecoolstuff.net>
2022-08-11 21:43:07 +1000

    installed bootstrap

--- app/javascript/components/Tickets/index.js
+++ app/javascript/components/Tickets/index.js
@@ -1,15 +1,20 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Ticket from "./Ticket.js"
+import Button from "react-bootstrap/Button"
+import { Container } from "react-bootstrap"
 
 const TicketsPage = (props) => {
   const tickets = props.tickets
   console.log(tickets)
   return (
     <>
-      {
-        tickets.map((ticket) => <Ticket key={ticket.id} {...ticket} /> )
-      }
+      <Container>
+        {
+          tickets.map((ticket) => <Ticket key={ticket.id} {...ticket} /> )
+        }
+        <Button>Nice</Button>
+      </Container>
     </>
   );
 }
